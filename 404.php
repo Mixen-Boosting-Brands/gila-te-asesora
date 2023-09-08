23b189a60b449cd27097f55680b37f7f64f58878
Luis Carlos Pando <hey@luiscarlospando.com>
2023-09-08 12:17:06 -0600

    Armar plantillas

--- 404.php
+++ 404.php
@@ -1,24 +1,24 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
+	<section data-aos="fade-in" data-aos-duration="1000" id="featured-img" style="background: url('<?php echo get_the_post_thumbnail_url( null, 'full' ); ?>');">
+		<div class="bg-gradient"></div>
+	</section>
 
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php esc_html_e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<section data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400" id="contenido">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="bg-contenido">
+						<div class="row">
+							<div class="col-10 offset-1 col-lg-9 offset-lg-2">
+								<h1>Error 404: Página no encontrada</h1>
+								<p>La página solicitada no se encontró en el servidor, probablemente cambió de lugar o fue eliminada.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,45 +1,33 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
+	<section data-aos="fade-in" data-aos-duration="1000" id="featured-img" style="background: url('<?php echo get_the_post_thumbnail_url( null, 'full' ); ?>');">
+        <div class="bg-gradient"></div>
+	</section>
 
-			<h1><?php the_title(); ?></h1>
-
-		<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments. ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+    <section data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400" id="contenido">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="bg-contenido">
+                        <div class="row">
+                            <div class="col-10 offset-1 col-lg-1 ofsset-lg-0">
+                                <!-- Aquí va social sharing -->
+								<?php if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) { ADDTOANY_SHARE_SAVE_KIT(); } ?>
+                            </div>
+                            <div class="col-10 offset-1 col-lg-9 offset-lg-0">
+							<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
+                                <h1><?php the_title(); ?></h1>
+                                <?php the_content(); ?>
+								<?php edit_post_link(); // Always handy to have Edit Post Links available. ?>
+							<?php endwhile; ?>
+							<?php else: ?>
+							<?php endif; ?>
+							</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
